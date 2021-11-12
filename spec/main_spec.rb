5f04274262ce88b812082fdd67b6bff8e5f7c51d
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2021-11-12 12:11:30 +0300

    Fix minitest deprecations

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -25,92 +25,92 @@ describe "Minitest coverage" do
 
   it "works" do
     res, code = run_dummy_test 'dummy_test.rb'
-    code.success?.must_equal true
-    res.must_match /\d+ assertions, 0 failures, 0 errors, 0 skips/
-    res.must_match /Routes coverage is (\d+(.\d+)?)%/
+    _(code.success?).must_equal true
+    _(res).must_match /\d+ assertions, 0 failures, 0 errors, 0 skips/
+    _(res).must_match /Routes coverage is (\d+(.\d+)?)%/
   end
 
   it "has route listing" do
-    res, code = run_dummy_test 'dummy_test_full.rb'
-    code.success?.must_equal true
-    res.must_match %r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}
-    res.must_match /dummy#update/
+    res, code = run_dummy_test('dummy_test_full.rb')
+    _(code.success?).must_equal true
+    _(res).must_match %r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}
+    _(res).must_match /dummy#update/
   end
 
   it "does not count catch-all routes" do
     res, code = run_dummy_test 'dummy_test_full.rb'
-    code.success?.must_equal true
-    res.must_match %r{dummy#not_found_error}
-    res.wont_match %r{dummy#not_found_error\s+\d}
+    _(code.success?).must_equal true
+    _(res).must_match %r{dummy#not_found_error}
+    _(res).wont_match %r{dummy#not_found_error\s+\d}
   end
 
   it "has namespace filters" do
     res, code = run_dummy_test 'dummy_test_nsfilters.rb'
-    code.success?.must_equal true
-    res.must_match %r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}
-    res.wont_match %r{/somespace/}
-    res.must_match %r{/otherspace/}
+    _(code.success?).must_equal true
+    _(res).must_match %r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}
+    _(res).wont_match %r{/somespace/}
+    _(res).must_match %r{/otherspace/}
   end
 
   it "has regex filters" do
     res, code = run_dummy_test 'dummy_test_refilters.rb'
-    code.success?.must_equal true
-    res.must_match %r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}
-    res.wont_match %r{/PATCH/}
-    res.wont_match %r{/index/}
+    _(code.success?).must_equal true
+    _(res).must_match %r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}
+    _(res).wont_match %r{/PATCH/}
+    _(res).wont_match %r{/index/}
   end
 
   it "generates html report" do
     res, code = run_dummy_test 'dummy_html.rb'
-    code.success?.must_equal true
-    File.file?('coverage/routes.html').must_equal true
+    _(code.success?).must_equal true
+    _(File.file?('coverage/routes.html')).must_equal true
   end
 
   it "filters fallback put-routes" do
     skip if Rails.version < '4'
     res, code = run_dummy_test 'put_test.rb'
-    code.success?.must_equal true
-    res.must_match %r|PUT\s+/standalone_put|
-    res.must_match %r|PUT\s+/used_put/:|
-    res.wont_match %r|PUT\s+/reqs/:|
+    _(code.success?).must_equal true
+    _(res).must_match %r|PUT\s+/standalone_put|
+    _(res).must_match %r|PUT\s+/used_put/:|
+    _(res).wont_match %r|PUT\s+/reqs/:|
   end
 
   it "supports redirect routes" do
     res, code = run_dummy_test 'redirect_test.rb'
-    code.success?.must_equal true
-    res.must_match %r|GET\s+/|
-    res.must_match %r|Routes coverage is 50|
-    res.must_match %r|1 of 2 routes hit|
+    _(code.success?).must_equal true
+    _(res).must_match %r|GET\s+/|
+    _(res).must_match %r|Routes coverage is 50|
+    _(res).must_match %r|1 of 2 routes hit|
   end
 
   it "constraints_differ" do
     res, code = run_dummy_test 'constraints_test.rb'
-    code.success?.must_equal true
-    res.must_match %r|GET\s+/rec\(\.:format\)\s+dummy#index\s+1|
-    res.must_match %r|GET\s+/rec\(\.:format\)\s+dummy#update\s+1|
-    res.must_match %r|GET\s+/rec/:TYPE\(\.:format\)\s+dummy#current\s+1|
+    _(code.success?).must_equal true
+    _(res).must_match %r|GET\s+/rec\(\.:format\)\s+dummy#index\s+1|
+    _(res).must_match %r|GET\s+/rec\(\.:format\)\s+dummy#update\s+1|
+    _(res).must_match %r|GET\s+/rec/:TYPE\(\.:format\)\s+dummy#current\s+1|
   end
 
   it "working mounted engines, including Sprockets" do
     skip if Rails.version < '4'
     res, code = run_dummy_test 'sprockets_test.rb'
-    code.success?.must_equal true
+    _(code.success?).must_equal true
   end
 
   if defined? RSpec
     it "works with rspec" do
       res,code = run_dummy_rspec 'dummy_rspec.rb'
-      code.success?.must_equal true
-      res.must_match /Routes coverage is (\d+(.\d+)?)%/
+      _(code.success?).must_equal true
+      _(res).must_match /Routes coverage is (\d+(.\d+)?)%/
     end
   end
 
   if defined? SimpleCov
     it "works with simplecov" do
       res,code = run_dummy_test 'dummy_test_simplecov.rb'
-      code.success?.must_equal true
-      res.must_match /Routes coverage is (\d+(.\d+)?)%/
-      res.must_match /routes\.html/
+      _(code.success?).must_equal true
+      _(res).must_match /Routes coverage is (\d+(.\d+)?)%/
+      _(res).must_match /routes\.html/
       # File.file?(?../routes.html).must_equal true
     end
   end
