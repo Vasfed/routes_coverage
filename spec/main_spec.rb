a7d565372cacddc516cc10ac909ddeb50ebe1bbc
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2022-04-24 17:28:59 +0300

    Use more compatible minitest syntax

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -5,6 +5,7 @@ require 'English'
 Bundler.require(:default)
 
 require 'minitest/autorun'
+require 'minitest/spec'
 
 describe "Minitest coverage" do
   def run_cmd(cmd)
@@ -27,117 +28,117 @@ describe "Minitest coverage" do
 
   it "works" do
     res, code = run_dummy_test 'dummy_test.rb'
-    _(code.success?).must_equal true
-    _(res).must_match(/\d+ assertions, 0 failures, 0 errors, 0 skips/)
-    _(res).must_match(/Routes coverage is (\d+(.\d+)?)%/)
+    assert(code.success?)
+    assert_match(/\d+ assertions, 0 failures, 0 errors, 0 skips/, res)
+    assert_match(/Routes coverage is (\d+(.\d+)?)%/, res)
   end
 
   it "has route listing" do
     res, code = run_dummy_test('dummy_test_full.rb')
-    _(code.success?).must_equal true
-    _(res).must_match %r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}
-    _(res).must_match(/dummy#update/)
+    assert(code.success?)
+    assert_match(%r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}, res)
+    assert_match(/dummy#update/, res)
   end
 
   it "does not count catch-all routes" do
     res, code = run_dummy_test 'dummy_test_full.rb'
-    _(code.success?).must_equal true
-    _(res).must_match(/dummy#not_found_error/)
-    _(res).wont_match(/dummy#not_found_error\s+\d/)
+    assert(code.success?)
+    assert_match(/dummy#not_found_error/, res)
+    refute_match(/dummy#not_found_error\s+\d/, res)
   end
 
   it "has namespace filters" do
     res, code = run_dummy_test 'dummy_test_nsfilters.rb'
-    _(code.success?).must_equal true
-    _(res).must_match %r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}
-    _(res).wont_match %r{/somespace/}
-    _(res).must_match %r{/otherspace/}
+    assert(code.success?)
+    assert_match(%r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}, res)
+    refute_match(%r{/somespace/}, res)
+    assert_match(%r{/otherspace/}, res)
   end
 
   it "has regex filters" do
     res, code = run_dummy_test 'dummy_test_refilters.rb'
-    _(code.success?).must_equal true
-    _(res).must_match %r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}
-    _(res).wont_match %r{/PATCH/}
-    _(res).wont_match %r{/index/}
+    assert(code.success?)
+    assert_match(%r{POST\s+/reqs/current\(\.:format\)\s+dummy#current\s+1}, res)
+    refute_match(%r{/PATCH/}, res)
+    refute_match(%r{/index/}, res)
   end
 
   it "generates html report" do
     res, code = run_dummy_test 'dummy_html.rb'
-    _(code.success?).must_equal true
-    _(res).must_include "coverage/routes.html"
-    _(File.file?('coverage/routes.html')).must_equal true
+    assert(code.success?)
+    assert_includes(res, "coverage/routes.html")
+    assert(File.file?('coverage/routes.html'))
   end
 
   it "filters fallback put-routes" do
     skip if Rails.version < '4'
     res, code = run_dummy_test 'put_test.rb'
-    _(code.success?).must_equal true
-    _(res).must_match %r{PUT\s+/standalone_put}
-    _(res).must_match %r{PUT\s+/used_put/:}
-    _(res).wont_match %r{PUT\s+/reqs/:}
+    assert(code.success?)
+    assert_match(%r{PUT\s+/standalone_put}, res)
+    assert_match(%r{PUT\s+/used_put/:}, res)
+    refute_match(%r{PUT\s+/reqs/:}, res)
   end
 
   it "supports redirect routes" do
     res, code = run_dummy_test 'redirect_test.rb'
-    _(code.success?).must_equal true
-    _(res).must_match %r{GET\s+/}
-    _(res).must_match(/Routes coverage is 50/)
-    _(res).must_match(/1 of 2 routes hit/)
+    assert(code.success?)
+    assert_match(%r{GET\s+/}, res)
+    assert_match(/Routes coverage is 50/, res)
+    assert_match(/1 of 2 routes hit/, res)
   end
 
   it "constraints_differ" do
     res, code = run_dummy_test 'constraints_test.rb'
-    _(code.success?).must_equal true
-    _(res).must_match %r{GET\s+/rec\(\.:format\)\s+dummy#index\s+1}
-    _(res).must_match %r{GET\s+/rec\(\.:format\)\s+dummy#update\s+1}
-    _(res).must_match %r{GET\s+/rec/:TYPE\(\.:format\)\s+dummy#current\s+1}
+    assert(code.success?)
+    assert_match(%r{GET\s+/rec\(\.:format\)\s+dummy#index\s+1}, res)
+    assert_match(%r{GET\s+/rec\(\.:format\)\s+dummy#update\s+1}, res)
+    assert_match(%r{GET\s+/rec/:TYPE\(\.:format\)\s+dummy#current\s+1}, res)
   end
 
   it "groups" do
     res, code = run_dummy_test 'dummy_test_groups.rb'
-    _(code.success?).must_equal true
-    _(res).must_include "Some group: 0.0% (0 of 1 routes hit)"
-    _(res).must_include "Foo: 33.3% (1 of 3 routes hit at 1.0 hits average)"
-    _(res).must_include "Subdomain: 0.0% (0 of 1 routes hit)"
-    _(res).must_include "Ungroupped: 33.3% (1 of 3 routes hit at 1.0 hits average)"
+    assert(code.success?)
+    assert_includes(res, "Some group: 0.0% (0 of 1 routes hit)")
+    assert_includes(res, "Foo: 33.3% (1 of 3 routes hit at 1.0 hits average)")
+    assert_includes(res, "Subdomain: 0.0% (0 of 1 routes hit)")
+    assert_includes(res, "Ungroupped: 33.3% (1 of 3 routes hit at 1.0 hits average)")
   end
 
   it "working mounted engines, including Sprockets" do
     skip if Rails.version < '4'
     res, code = run_dummy_test 'sprockets_test.rb'
-    _(code.success?).must_equal true
-    _(res).must_match(/Routes coverage is (\d+(.\d+)?)%/)
+    assert(code.success?)
+    assert_match(/Routes coverage is (\d+(.\d+)?)%/, res)
   end
 
   it "inferring coverage by controller tests" do
     ENV['INFER_FROM_CONTROLLER'] = '1'
     res, code = run_dummy_test 'dummy_controller_test.rb'
     route_regex = %r{GET\s+/reqs\(\.:format\)\s+dummy#index\s+1}
-    _(code.success?).must_equal true
-    _(res).must_match(route_regex)
+    assert(code.success?)
+    assert_match(route_regex, res)
 
     ENV['INFER_FROM_CONTROLLER'] = '0'
     res, code = run_dummy_test 'dummy_controller_test.rb'
-    _(code.success?).must_equal true
-    _(res).wont_match(route_regex)
+    assert(code.success?)
+    refute_match(route_regex, res)
   end
 
   if defined? RSpec
     it "works with rspec" do
       res, code = run_dummy_rspec 'dummy_rspec.rb'
-      _(code.success?).must_equal true
-      _(res).must_match(/Routes coverage is (\d+(.\d+)?)%/)
+      assert(code.success?)
+      assert_match(/Routes coverage is (\d+(.\d+)?)%/, res)
     end
   end
 
   if defined? SimpleCov
     it "works with simplecov" do
       res, code = run_dummy_test 'dummy_test_simplecov.rb'
-      _(code.success?).must_equal true
-      _(res).must_match(/Routes coverage is (\d+(.\d+)?)%/)
-      _(res).must_match(/routes\.html/)
-      # File.file?(?../routes.html).must_equal true
+      assert(code.success?)
+      assert_match(/Routes coverage is (\d+(.\d+)?)%/, res)
+      assert_match(/routes\.html/, res)
+      # assert(File.file?(?../routes.html))
     end
   end
 end
